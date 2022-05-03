b7ee504e9c176262431516164469f59115b18564
Jose Luis Sánchez Carrasco <joseluischezcarrasco@gmail.com>
2022-05-03 10:32:06 +0200

    Creating class Objeto
    Al final la clase Objeto no se tiene que incluir en AppPokemon asi que todo lo relacionado con Objeto no vale para nada

--- src/main/java/model/ModelUtils.java
+++ src/main/java/model/ModelUtils.java
@@ -1,5 +1,6 @@
 package model;
 
+import eu.iamgio.pokedex.item.ItemHold;
 import eu.iamgio.pokedex.pokemon.PokemonType;
 import eu.iamgio.pokedex.pokemon.Stat;
 import eu.iamgio.pokedex.util.Pair;
@@ -15,11 +16,14 @@ public class ModelUtils {
         int ataqueEspecial = pk.getStat(Stat.Type.SPECIAL_ATTACK).getBaseStat();
         int defensaEspecial = pk.getStat(Stat.Type.SPECIAL_DEFENSE).getBaseStat();
         int velocidad = pk.getStat(Stat.Type.SPEED).getBaseStat();
-        int precision = pk.getStat(Stat.Type.ACCURACY).getBaseStat();
-        int evasion = pk.getStat(Stat.Type.EVASION).getBaseStat();
         int experiencia = pk.getBaseExperience();
         Pair<PokemonType, PokemonType> tipos = pk.getTypes();
 
-        return new Pokemon(id, nombre, vitalidad, ataque, defensa, ataqueEspecial, defensaEspecial, velocidad, precision, evasion, experiencia, tipos);
+        return new Pokemon(id, nombre, vitalidad, ataque, defensa, ataqueEspecial, defensaEspecial, velocidad, experiencia, tipos);
+    }
+
+    public static ObjetoPokemon parseItemsPokemon(ItemHold item){
+        //TODO
+        return new ObjetoPokemon();
     }
 }
