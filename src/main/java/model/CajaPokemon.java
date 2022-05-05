8cc29e7beb18b5fe85339788d815c3f6adb964ca
Jose Luis Sánchez Carrasco <joseluischezcarrasco@gmail.com>
2022-05-05 09:28:48 +0200

    Created CajaPokemon

--- src/main/java/model/CajaPokemon.java
+++ src/main/java/model/CajaPokemon.java
@@ -1,5 +1,26 @@
 package model;
 
-public class CajaPokemon {
+import java.util.LinkedList;
+import java.util.List;
 
+public class CajaPokemon {
+    private int id;
+    private List<Pokemon> pokemons;
+
+    public CajaPokemon(){
+        id = 0;
+        pokemons = new LinkedList<>();
+    }
+
+    public boolean isPokemonInCaja(Pokemon pokemon){
+        return pokemons.contains(pokemon);
+    }
+
+    public void addPokemon(Pokemon pokemon){
+        pokemons.add(pokemon);
+    }
+
+    public void removePokemon(Pokemon pokemon){
+        pokemons.remove(pokemon);
+    }
 }
