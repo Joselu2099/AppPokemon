76b6c0ae32b4af637d8cc8cc03a82961ec49ec5a
Jose Luis Sánchez Carrasco <38717344+Joselu2099@users.noreply.github.com>
2022-05-08 10:46:14 +0200

    Table Pokemon Created

--- src/main/java/launcher/Launcher.java
+++ src/main/java/launcher/Launcher.java
@@ -5,9 +5,6 @@ import model.PokemonRepository;
 
 public class Launcher {
     public static void main(String[] args) {
-        PokemonRepository.getINSTANCE();
         MovimientosRepository.getINSTANCE().getMovimientos().forEach(mv -> System.out.println(mv.getNombre()));
-
-
     }
 }
