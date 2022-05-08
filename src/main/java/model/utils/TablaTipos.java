19e57e4dbf92eaac9de6c135c1e1e069c3b7894b
Jose Luis Sánchez Carrasco <38717344+Joselu2099@users.noreply.github.com>
2022-05-08 10:24:36 +0200

    TablaTipos implemented && JUnit Test

--- src/main/java/model/utils/TablaTipos.java
+++ src/main/java/model/utils/TablaTipos.java
@@ -1,7 +1,9 @@
 package model.utils;
 
+import eu.iamgio.pokedex.pokemon.PokemonType;
+
 public class TablaTipos {
-    public static double[][] efectividadesPokemon = {
+    private static final double[][] efectividadesPokemon = {
             /*ACERO   AGUA   BICHO   DRAGON   ELÉC   FANT   FUEGO   HADA   HIELO   LUCHA   NORMAL   PLANTA   PSI   ROCA   SINIE  TIERRA   VENENO   VOLADOR   */
 
             { 0.5   , 0.5  ,   1   ,   1    , 0.5  ,  1   ,  0.5   ,  2  ,   2   ,   1   ,    1    ,   1   ,  1   ,  2  ,   1   ,   1   ,    1   ,   1},   //ACERO
@@ -23,4 +25,52 @@ public class TablaTipos {
             {  0    ,  1   ,   1   ,   1    ,  1   , 0.5  ,   1    ,  2  ,   1   ,   1   ,    1    ,   2   ,  1   , 0.5 ,   1   ,  0.5  ,   0.5  ,   1},   // VENENO
             { 0.5   ,  1   ,   2   ,   1    , 0.5  ,  1   ,   1    ,  1  ,   1   ,   2   ,    1    ,   2   ,  1   , 0.5 ,   1   ,   1   ,    1   ,   1}    // VOLADOR
     };
+
+    public static double getEfectividad(PokemonType tipoAtacante, PokemonType tipoRival){
+        return efectividadesPokemon[getIndex(tipoAtacante)][getIndex(tipoRival)];
+    }
+
+    public static int getIndex(PokemonType type){
+        switch (type){
+            case STEEL:
+                return 0;
+            case WATER:
+                return 1;
+            case BUG:
+                return 2;
+            case DRAGON:
+                return 3;
+            case ELECTRIC:
+                return 4;
+            case GHOST:
+                return 5;
+            case FIRE:
+                return 6;
+            case FAIRY:
+                return 7;
+            case ICE:
+                return 8;
+            case FIGHTING:
+                return 9;
+            case NORMAL:
+                return 10;
+            case GRASS:
+                return 11;
+            case PSYCHIC:
+                return 12;
+            case ROCK:
+                return 13;
+            case DARK:
+                return 14;
+            case GROUND:
+                return 15;
+            case POISON:
+                return 16;
+            case FLYING:
+                return 17;
+            case UNKNOWN:
+            default:
+                return -1;
+        }
+    }
 }
---
+++ src/test/java/model/utils/TablaTiposTest.java
@@ -0,0 +1,17 @@
+package model.utils;
+
+import eu.iamgio.pokedex.pokemon.PokemonType;
+import junit.framework.TestCase;
+
+public class TablaTiposTest extends TestCase {
+
+    public void testGetEfectividad() {
+        assertEquals(TablaTipos.getEfectividad(PokemonType.FIRE, PokemonType.STEEL), 2.0);
+        assertEquals(TablaTipos.getEfectividad(PokemonType.NORMAL, PokemonType.ROCK), 0.5);
+    }
+
+    public void testGetIndex() {
+        assertEquals(TablaTipos.getIndex(PokemonType.WATER), 1);
+        assertEquals(TablaTipos.getIndex(PokemonType.PSYCHIC), 12);
+    }
+}
