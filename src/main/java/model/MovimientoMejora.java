b313fb7c9ba318498e0f41b912c9057b816ed1a5
Jose Luis Sánchez Carrasco <joseluischezcarrasco@gmail.com>
2022-05-09 14:10:40 +0200

    Refactored Movimiento
    numTurno no es necesario como atributo de Movimiento, puede ser una propiedad calculada

--- src/main/java/model/MovimientoMejora.java
+++ src/main/java/model/MovimientoMejora.java
@@ -1,22 +1,20 @@
 package model;
 
 import eu.iamgio.pokedex.pokemon.PokemonType;
+import model.utils.ModelUtils;
 
 public class MovimientoMejora extends Movimiento {
 
     private int mejora;
-    private int numTurnos;
 
     public MovimientoMejora() {
         super();
         this.mejora = 0;
-        this.numTurnos = 0;
     }
 
-    public MovimientoMejora(String nombre, PokemonType tipo, int mejora, int numTurnos) {
+    public MovimientoMejora(String nombre, PokemonType tipo, int mejora) {
         super(nombre, tipo);
         this.mejora = mejora;
-        this.numTurnos = numTurnos;
     }
 
     public int getMejora() {
@@ -27,16 +25,16 @@ public class MovimientoMejora extends Movimiento {
         this.mejora = mejora;
     }
 
+    /**
+     * Num_turnos es una propiedad calculada, de forma random una mejora podra durar entre 1 o 5 turnos.
+     * @return numero de turnos que dura la mejora o que se realiza la mejora
+     */
     public int getNumTurnos() {
-        return numTurnos;
-    }
-
-    public void setNumTurnos(int numTurnos) {
-        this.numTurnos = numTurnos;
+        return ModelUtils.generarNumRandom(1,5);
     }
 
     public int consumoEstamina() {
-        return numTurnos*10;
+        return getNumTurnos()*10;
     }
 
 }
